4f2f83982b72f06b3daa1979c2c0b3ff6bdaf21d
Lorans <elornorthwind@gmail.com>
2023-01-23 22:09:18 +0300

    fixed the business logic

--- src/services/roomsCalculateService.js
+++ src/services/roomsCalculateService.js
@@ -1,80 +1,3 @@
-// export function updateRoomsInfo(rooms) {
-//   const sMainSum = rooms.reduce((acc, cur) => acc + cur.sMain, 0);
-//   const sCommonSecondarySum = rooms
-//     .filter((i) => !i.blockNum)
-//     .reduce((acc, cur) => acc + cur.sSecondary, 0);
-//   const sCommonSummerSum = rooms
-//     .filter((i) => !i.blockNum)
-//     .reduce((acc, cur) => acc + cur.sSecondary, 0);
-
-//   const mapRooms = (item, index, arr) => {
-//     const blockLength = arr.filter((i) => i.blockNum === item.blockNum).length;
-
-//     let rowStyle = "common";
-//     if (
-//       item.blockNum &&
-//       (index === 0 || arr[index - 1].blockNum !== item.blockNum)
-//     ) {
-//       rowStyle = "block__first";
-//     } else if (item.blockNum) {
-//       rowStyle = "block";
-//     }
-
-//     const sBlockMainSum = arr
-//       .filter((i) => i.blockNum === item.blockNum)
-//       .reduce((acc, cur) => acc + cur.sMain, 0);
-
-//     const sBlockSecondarySum = arr
-//       .filter((i) => i.blockNum === item.blockNum)
-//       .reduce((acc, cur) => acc + cur.sSecondary, 0);
-
-//     const sBlockSummerSum = arr
-//       .filter((i) => i.blockNum === item.blockNum)
-//       .reduce((acc, cur) => acc + cur.sSummer, 0);
-
-//     const sBlockShare = sBlockMainSum / sMainSum;
-
-//     const sBlockObsh =
-//       sBlockShare * sCommonSecondarySum + sBlockMainSum + sBlockSecondarySum;
-
-//     const sBlockZhP =
-//       sBlockObsh + sBlockSummerSum + sBlockShare * sCommonSummerSum;
-
-//     console.log(sBlockObsh, sBlockZhP);
-
-//     return {
-//       ...item,
-//       rowStyle: rowStyle,
-//       rowSpawn: rowStyle === "block__first" ? blockLength : 0,
-//       sBlockMainSum,
-//       sBlockObsh,
-//       sBlockZhP,
-//       formated: {
-//         sMain: sFormat(item.sMain),
-//         sSecondary: sFormat(item.sSecondary),
-//         sSummer: sFormat(item.sSummer),
-//         sBlockMainSum: sFormat(sBlockMainSum),
-//         sBlockObsh: sFormat(sBlockObsh),
-//         sBlockZhP: sFormat(sBlockZhP),
-//       },
-//     };
-//   };
-
-//   return rooms.map(mapRooms);
-// }
-
-function sFormat(s) {
-  return s === 0 ? null : s.toFixed(1);
-}
-
-export function initialSortRooms(a, b) {
-  if (a.num === b.num) {
-    return a.letter < b.letter ? -1 : 1;
-  } else {
-    return a.num < b.num ? -1 : 1;
-  }
-}
-
 export function roomsToBlocks(rooms) {
   let blocks = [];
   let commonBlock = 1;
@@ -124,7 +47,7 @@ function saturateBlocks(rooms, blocks) {
     .reduce((acc, cur) => acc + cur.sSecondary, 0);
   const sCommonSummerSum = rooms
     .filter((i) => !i.blockNum)
-    .reduce((acc, cur) => acc + cur.sSecondary, 0);
+    .reduce((acc, cur) => acc + cur.sSummer, 0);
 
   function addAreaInfo(item) {
     const sBlockMainSum = rooms
@@ -151,4 +74,16 @@ function saturateBlocks(rooms, blocks) {
   }
 
   return blocks.map(addAreaInfo);
+}
+
+function sFormat(s) {
+  return s === 0 ? null : s.toFixed(1);
+}
+
+export function initialSortRooms(a, b) {
+  if (a.num === b.num) {
+    return a.letter < b.letter ? -1 : 1;
+  } else {
+    return a.num < b.num ? -1 : 1;
+  }
 }
