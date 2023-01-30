cb7ff0534729d33a47492475687001f006c9c75a
Lorans <elornorthwind@gmail.com>
2023-01-30 20:49:30 +0300

    error handling and excel file validation

--- src/services/excelReadService.js
+++ src/services/excelReadService.js
@@ -1,11 +1,13 @@
 import Excel from "exceljs";
+import { setRoomsStatusAction } from "../store/roomsReducer";
 
-export async function readWorksheet(buffer) {
+export async function readWorksheet(buffer, dispatch) {
   try {
     const wb = new Excel.Workbook();
     const workbook = await wb.xlsx.load(buffer);
     return workbook.getWorksheet(1);
   } catch (e) {
     console.log(e);
+    dispatch(setRoomsStatusAction("error"));
   }
 }
--- src/services/fileService.js
+++ src/services/fileService.js
@@ -1,10 +1,13 @@
-export function readFile(file) {
+import { setRoomsStatusAction } from "../store/roomsReducer";
+
+export function readFile(file, dispatch) {
   return new Promise((res, rej) => {
     const reader = new FileReader();
     reader.onload = () => {
       res(reader.result);
     };
     reader.onerror = () => {
+      dispatch(setRoomsStatusAction("error"));
       rej(reader.statusText);
     };
     reader.readAsArrayBuffer(file);
